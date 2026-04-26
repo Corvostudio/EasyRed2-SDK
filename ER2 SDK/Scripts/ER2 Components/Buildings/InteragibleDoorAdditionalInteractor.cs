f6344d62f9f0e13f686c28281f33ce10c70ca819
corvostudio <marcoamadei1998@gmail.com>
2026-04-26 20:56:44 +0200

    Interagible door auto setup

--- ER2 SDK/Scripts/ER2 Components/Buildings/InteragibleDoorAdditionalInteractor.cs
+++ ER2 SDK/Scripts/ER2 Components/Buildings/InteragibleDoorAdditionalInteractor.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public partial class InteragibleDoorAdditionalInteractor : Interagible
 {
+    public InteragibleDoor connectedDoor;
 
+#if UNITY_EDITOR
+    private void Awake()
+    {
+        if (!connectedDoor)
+            connectedDoor = GetComponentInParent<InteragibleDoor>();
+    }
+#endif
 }
