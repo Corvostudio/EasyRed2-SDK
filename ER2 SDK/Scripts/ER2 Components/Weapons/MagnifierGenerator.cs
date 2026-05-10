593ccf3d3811627cd393d0a881bfda7b9a24ff72
corvostudio <marcoamadei1998@gmail.com>
2026-05-10 12:15:46 +0200

    Fixed magnifier generator

--- ER2 SDK/Scripts/ER2 Components/Weapons/MagnifierGenerator.cs
+++ ER2 SDK/Scripts/ER2 Components/Weapons/MagnifierGenerator.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public partial class MagnifierGenerator : MonoBehaviour
 {
-    public new Camera camera;
+    [Header("Scope sprite image")]
+    [Tooltip("Set as sprite on import settings")]
+    public Texture scopeSprite;
+
+    [Header("Don't edit below")]
+    public MeshRenderer glass;
+    //public new Camera camera;
 }
