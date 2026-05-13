1f850152ba5011137e1b4a09068ece7c492a86dd
corvostudio <marcoamadei1998@gmail.com>
2026-05-13 16:17:33 +0200

    Added trailer functions

---
+++ ER2 SDK/Scripts/ER2 Components/Veichles/SimpleWheelUpdater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public partial class SimpleWheelUpdater : MonoBehaviour
+{
+    public Vehicle vehicle;
+
+    [Range(-70, 70)]
+    public float steer_angle = 0;
+    public float wheel_radius = .3f;
+
+    public Transform[] wheelsTransform_syncOnlyRot;
+
+}
