d00ecbb92fd63a01a8b32817b1d0b9d6fb94cf0e
corvostudio <marcoamadei1998@gmail.com>
2026-05-19 17:07:50 +0200

    Improved sdk

--- ER2 SDK/Scripts/ER2 Components/Generic/TerrainDetailData.cs
+++ ER2 SDK/Scripts/ER2 Components/Generic/TerrainDetailData.cs
@@ -2,6 +2,9 @@
 using UnityEngine;
 public partial class TerrainDetailData : MonoBehaviour
 {
+    [Range(0,1)]
+    public float alignToGround = 0;
+
     //public TerrainDetailType type;
     public Vector2 minMaxWidth = new Vector2(0.7f, 1.6f);
     public Vector2 minMaxHeight = new Vector2(0.7f, 1f);
