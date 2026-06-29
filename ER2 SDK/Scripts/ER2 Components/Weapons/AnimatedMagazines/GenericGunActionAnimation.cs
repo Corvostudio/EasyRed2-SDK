ba25301a56ee586209fc489c8a6ddac31af53256
corvostudio <marcoamadei1998@gmail.com>
2026-06-29 14:45:28 +0200

    cleanups

--- ER2 SDK/Scripts/ER2 Components/Weapons/AnimatedMagazines/GenericGunActionAnimation.cs
+++ ER2 SDK/Scripts/ER2 Components/Weapons/AnimatedMagazines/GenericGunActionAnimation.cs
@@ -1,21 +1,7 @@
 using UnityEngine;
 
 
-public abstract class GenericGunActionAnimation : MonoBehaviour
+public abstract partial class GenericGunActionAnimation : MonoBehaviour
 {
-    public abstract void Play(GenericGun gun, Soldier soldier);
 
-    public virtual void StopAndReset(GenericGun gun)
-    {
-        StopAllCoroutines();
-    }
-
-    protected virtual void OnDisable()
-    {
-        StopAllCoroutines();
-    }
-    public virtual void PlayReload(GenericGun gun, Soldier soldier)
-    {
-
-    }
 }
--- ER2 SDK/Scripts/ER2 Components/Weapons/AnimatedMagazines/GenericGunAnimationClipRunner.cs
+++ ER2 SDK/Scripts/ER2 Components/Weapons/AnimatedMagazines/GenericGunAnimationClipRunner.cs
@@ -1,32 +1,10 @@
 using UnityEngine;
 
-public class GenericGunAnimationClipRunner : GenericGunActionAnimation
+public partial class GenericGunAnimationClipRunner : GenericGunActionAnimation
 {
     public Animation targetAnimation;
     public string animationName = "bolt_anim";
     public bool stopBeforePlay = false;
     public bool rewindBeforePlay = false;
 
-    public override void Play(GenericGun gun, Soldier soldier)
-    {
-        if (!isActiveAndEnabled || string.IsNullOrEmpty(animationName))
-            return;
-
-        if (!targetAnimation)
-            targetAnimation = GetComponent<Animation>();
-
-        if (!targetAnimation && gun)
-            targetAnimation = gun.GetComponent<Animation>();
-
-        if (!targetAnimation)
-            return;
-
-        if (stopBeforePlay)
-            targetAnimation.Stop();
-
-        if (rewindBeforePlay)
-            targetAnimation.Rewind(animationName);
-
-        targetAnimation.Play(animationName);
-    }
 }
